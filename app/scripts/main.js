02e551845548e38da9fd90b41c257c690b7fda5e
Roman Velic <roman.velic@gmail.com>
2015-03-17 00:27:11 +0100

    basic js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,34 @@
-console.log('\'Allo \'Allo!');
+/* jshint devel:true */
+'use strict';
+
+if (typeof jQuery === 'undefined') {
+  throw new Error('This ninja requires jQuery');
+}
+
++function ($, moment) {
+
+  var date = $('.date')
+    , time = $('.time')
+    , zone = $('.zone')
+    , refresh
+    , now;
+
+  +function start() {
+    refresh = setTimeout(function() {
+	    now = moment();
+	    date.val(now.format('MMMM Do'));
+	    time.val(now.format('HH:mm:ss'));
+	    zone.val(now.format('Z z'));
+	    start();
+    }, 500);
+  }();
+
+  var onClickHandler = function onClickHandler() {
+  	clearTimeout(refresh);
+  };
+
+  date.on('click', onClickHandler);
+  time.on('click', onClickHandler);
+  zone.on('click', onClickHandler);
+
+}(jQuery, moment);
